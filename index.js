618698e991ba0ae03a91b9369e1fa13c824a7e9f
Jeremey Hansen <jeremeyhansen@yahoo.com>
2022-08-16 10:03:54 -0400

    commit tree 410f61d1af3adfb5784ca9c8983799a3ac0abfc4

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  let reversedString = word.split('').reverse().join('');
+  if (reversedString === word){
+    return true
+  } else {
+    return false};
 }
-
-/* 
-  Add your pseudocode here
-*/
 
 /*
   Add written explanation of your solution here
 */
 
+//well the first part is me declaring variable and splitting it, reversing it, and then joining it.
+//I then make an if else statement saying if reversed word equals inputted word than return true and else the opposite.
+
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome('susan'));
+
+
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
